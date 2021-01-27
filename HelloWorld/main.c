254322fec9b827430e0b2fada0dd90bd12eb9983
matt0975 <61263200+matt0975@users.noreply.github.com>
2021-01-27 10:46:26 -0500

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Matthew Wagner on 1/27/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
