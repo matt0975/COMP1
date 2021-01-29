3eda7d1b08a2cd730f044b8938bf6bf6a7b4ea10
matt0975 <61263200+matt0975@users.noreply.github.com>
2021-01-28 19:14:34 -0500

    Initial Commit

---
+++ Daily01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Daily01
+//
+//  Created by Matthew Wagner on 1/28/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
