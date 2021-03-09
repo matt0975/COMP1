764184eaa6451b396646eebb65d25942b77bf048
Matthew Wagner <matthew.wagner0975@gmail.com>
2021-03-08 23:56:50 -0500

    03/08/2021 Class Examples

--- HelloWorld/more_function1.c
+++ HelloWorld/more_function1.c
@@ -4,10 +4,12 @@
 //
 //  Created by Matt Wagner on 3/3/21.
 //
-
+/*
 #include <stdio.h>
 
 double calcSum(double userNum1, double userNum2, double userNum3);
+double inv(double num1);
+double getMin(double a, double b);
 
 int main(int argc, const char* argv[]){
     
@@ -18,6 +20,16 @@ int main(int argc, const char* argv[]){
     result = calcSum(num1, num2, num3);     // Result is the thing we are returning so we must assign it to the function
     printf("In main: result = %lf\n", result);
     
+     
+    // Find the Inverse of num
+    double num = 4.99, result;
+    result = inv(num);
+    printf("Multiplicatibe inverse of 4.99 is: %lf\n", result);
+    
+    
+    double x = 4.33, y = 0.34, result;
+    result = getMin(x, y);
+    printf("Result from getMin is: %lf", result);
     return 0;
 }
 
@@ -29,3 +41,21 @@ double calcSum(double userNum1, double userNum2, double userNum3)
     printf("In calcSum: sum = %lf", sum);
     return sum;
 }
+
+double inv(double num1)
+{
+    //double inverse;
+    //inverse = 1.0 / num1;
+    //return inverse;
+    return (1 / num1);
+}
+
+double getMin(double a, double b)
+{
+    //if(a >= b)
+       //return b;
+    //else
+        //return a;
+    return (a < b ? a : b); // ? compares the statement so if a<b is true you do that or false you return a or b depending on the minimum
+}
+*/
---
+++ HelloWorld/scope1.c
@@ -0,0 +1,37 @@
+//
+//  scope1.c
+//  HelloWorld
+//
+//  Created by Matt Wagner on 3/8/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char* argv[]){
+    /*
+    //outputs absolue value of i, from -50 to 50, but no neg since abs
+    for(int i = -50; i <= 50; i++){
+        printf("%d\n", abs(i));
+    }
+     
+    double x = 33.7, y = 12.1, sum, result;
+    
+    sum = sqrt(x) + sqrt(y);
+    printf("sqrt(x) = %lf, sqrt(y) = %lf, sum = %lf\n", x,y,sum);
+    
+    result = fabs(x);
+    printf("fabs(x) = %.3lf\n", result);
+    
+    double result = pow(5,2);
+    printf("pow(5,2) = %0.3lf\n", result);
+    
+    double result = floor(33.456);
+    printf("floor(33.456) = %lf\n", result);
+    double result2 = ceil(33.456);
+    printf("ceil(33.456) = %lf\n", result2);
+    */
+    
+    return 0;
+}
